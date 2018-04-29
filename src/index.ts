27cba791068059de072e55fd0f27cbbfbaf11512
Rodrigo Alejandro Juarez <rodrigo.juarez.inf@gmail.com>
2018-04-29 10:59:32 +0200

    Ellipse with p noise

--- src/index.ts
+++ src/index.ts
@@ -2,38 +2,26 @@
 import 'p5';
 var p5 = require('p5');
 
-import './assets/styles/main.scss';
 import './assets/js/p5.dom.js';
 
 var sketch = (p: p5) => {
   let canvas: any;
+  let t: number = 0;
   p.preload = () => {};
 
   p.setup = () => {
     (<any>window).p5 = p;
     canvas = p.createCanvas(400, 400);
-    p.background(100);
-    canvas.drop(gotFile);
   };
 
   p.draw = () => {
-    p.fill(p.color(255));
-    p.noStroke();
-    p.textSize(24);
-    p.textAlign(p.CENTER);
-    p.text('Drag an image file onto the canvas.', p.width / 2, p.height / 2);
-    p.noLoop();
-  };
+    const n = p.noise(t);
 
-  const gotFile = (file: any) => {
-    if (file.type === 'image') {
-      // Create an image DOM element but don't show it
-      var img = p.createImg(file.data);
-      // Draw the image onto the canvas
-      //p.image(img, 0, 0, p.width, p.height);
-    } else {
-      p.print('Not an image file!');
-    }
+    const x = p.map(n, 0, 1, 0, p.width);
+
+    p.ellipse(x, 180, 16, 16);
+
+    t += 0.01;
   };
 };
 
