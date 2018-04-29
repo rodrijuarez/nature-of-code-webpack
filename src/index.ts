09bcb4ed02c30cf60cfb4822b9016775e95b3130
Rodrigo Alejandro Juarez <rodrigo.juarez.inf@gmail.com>
2018-04-29 21:41:52 +0200

    Multiple balls rendering

--- src/index.ts
+++ src/index.ts
@@ -5,36 +5,58 @@ var p5 = require('p5');
 var sketch = (p: p5) => {
   let canvas: any;
 
-  let xspeed = 1;
-  let yspeed = 3.3;
+  let xSpeeds: number[] = [];
+  let ySpeeds: number[] = [];
+  let xCoordinates: number[] = [];
+  let yCoordinates: number[] = [];
 
-  let x = 0;
-  let y = 0;
+  let numberOfBalls = 50;
+  let xSpeed = 1;
+  let ySpeed = 3.3;
 
   p.preload = () => {};
 
   p.setup = () => {
     canvas = p.createCanvas(800, 800);
+    setupBalls();
+  };
+
+  const setupBalls = () => {
+    xCoordinates = Array(numberOfBalls).fill(0);
+    yCoordinates = Array(numberOfBalls).fill(0);
+    xSpeeds = Array(numberOfBalls).fill(xSpeed);
+    ySpeeds = Array(numberOfBalls).fill(ySpeed);
+
+    for (let index = 0; index < numberOfBalls; index++) {
+      xCoordinates[index] = p.random(0, p.width);
+      yCoordinates[index] = p.random(0, p.height);
+    }
   };
 
   p.draw = () => {
     p.background(p.color(255));
 
-    x += xspeed;
-    y += yspeed;
+    for (let index = 0; index < numberOfBalls; index++) {
+      calculateBallCoordinates(index);
+    }
+  };
 
-    if (x > p.width || x < 0) {
-      xspeed = xspeed * -1;
+  const calculateBallCoordinates = (index: number) => {
+    xCoordinates[index] += xSpeeds[index];
+    yCoordinates[index] += ySpeeds[index];
+
+    if (xCoordinates[index] > p.width || xCoordinates[index] < 0) {
+      xSpeeds[index] = xSpeeds[index] * -1;
     }
 
-    if (y > p.height || y < 0) {
-      yspeed = yspeed * -1;
+    if (yCoordinates[index] > p.height || yCoordinates[index] < 0) {
+      ySpeeds[index] = ySpeeds[index] * -1;
     }
 
     p.stroke(p.color(0));
     p.fill(p.color(175));
 
-    p.ellipse(x, y, 16, 16);
+    p.ellipse(xCoordinates[index], yCoordinates[index], 16, 16);
   };
 };
 
